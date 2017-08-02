ca8b542fd4448a632bbf3b8be584ae0dce6e11be
jacky4566 <jacksonwiebe@me.com>
2017-08-01 21:22:25 -0600

    Create ParticleCyclicalTimer.cpp

---
+++ ParticleCyclicalTimer.cpp
@@ -0,0 +1,67 @@
+/*
+  The MIT License (MIT)
+
+  Copyright (c) 2017 jacky4566
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of
+  this software and associated documentation files (the "Software"), to deal in
+  the Software without restriction, including without limitation the rights to
+  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+  the Software, and to permit persons to whom the Software is furnished to do so,
+  subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "ParticleCyclicalTimer.h"
+
+ //Initialize
+ cycleTimer::cycleTimer(byte ONhour, byte ONmin, byte OFFhour, byte OFFmin){
+	 _ONhour = ONhour;
+	 _ONmin  = ONmin;
+	 _OFFhour = OFFhour;
+	 _OFFmin  = OFFmin;
+ }
+ 
+ //Return true if timer is active
+ boolean cycleTimer::isActive(){
+	//get time since midnight
+	//Functions Time.Hour and Time.minute use Local time!
+	int _timeLocal = (Time.hour() * 3600) + (Time.minute() * 60);
+	//convert ON and OFF times to seconds from midnight
+	int _setOnFromMidnight  = ((int)_ONhour * 3600) + ((int)_ONmin * 60);
+	int _setOffFromMidnight = ((int)_OFFhour * 3600) + ((int)_OFFmin * 60);
+	
+	if(_setOnFromMidnight < _setOffFromMidnight)//Normal Routine
+		if(_timeLocal >= _setOnFromMidnight && _timeLocal <= _setOffFromMidnight){ //Time is between alarms
+			return true;
+		}
+		else
+			return false;
+	else if(_setOnFromMidnight > _setOffFromMidnight)//Inverted Routine
+		if(_timeLocal >= _setOnFromMidnight || _timeLocal <= _setOffFromMidnight){ //Time is outside alarms
+			return true;
+		}
+		else
+			return false;
+}
+ 
+//Set on time for this timer
+void  cycleTimer::setOnTime (byte hour, byte min){
+	_ONhour = hour;
+	_ONmin  = min;
+}
+	
+//Set off time for this timer
+void  cycleTimer::setOffTime (byte hour, byte min){
+	_OFFhour = hour;
+	_OFFmin  = min;
+}
